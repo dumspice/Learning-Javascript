e2c36fc2014e4febb00cfecb2057b500eb40f315
dumspice <126127927+dumspice@users.noreply.github.com>
2024-08-28 09:18:06 +0700

    add HTML DOM

---
+++ js_HTML_DOM/BAi1:HTML-DomLaGi/main.js
@@ -0,0 +1,11 @@
+//HTML DOM
+
+//Dom có 3 thành phần
+
+/**
+ * 1. Element: các thẻ HTML là element. Ví dụ: <head>, <body>, ...
+ * 2. Attribute: thuộc tính của các thẻ html. Ví dụ: href, src, ...
+ * 3. Text: nội dung của các thẻ.
+ */
+
+//Node: có thể là element, attribute hay text
---
+++ js_HTML_DOM/Bai2:HTMLDOM va DOMAPI/main.js
@@ -0,0 +1,16 @@
+//HTML DOM
+
+//Dom có 3 thành phần
+
+/**
+ * 1. Element
+ * 2. Attribute
+ * 3. Text
+ */
+
+// -------------------
+
+//JavaScript
+
+//Document Object
+document.write("<h1>hasodas</h1>");

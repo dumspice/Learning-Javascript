74724676534e4c59268ffab978aef0f6a446f5f5
dumspice <126127927+dumspice@users.noreply.github.com>
2025-03-03 16:11:51 +0700

    add dom event

--- js_HTML_DOM/DOM Events/main.js
+++ js_HTML_DOM/DOM Events/main.js
@@ -17,3 +17,36 @@ for (var index of h1Element) {
     console.log(e.target); //nên dùng e.target để tránh bị lỗi
   };
 }
+
+// Ví dụ
+
+// 1. Input / select
+
+var textElement = document.querySelector('input[type="text"]');
+
+textElement.oninput = (e) => {
+  console.log(e.target.value);
+};
+
+//oniput: nhập đến đâu lấy đến đấy
+
+var checkboxElement = document.querySelector('input[type="checkbox"]');
+
+checkboxElement.onchange = (e) => {
+  var checked = e.target.checked ? "checked" : "not checked";
+  console.log(checked);
+};
+
+var selectElement = document.querySelector("select");
+
+console.log(selectElement);
+
+selectElement.onchange = (e) => {
+  console.log(e.target.value);
+};
+
+// 2. Key up / down
+
+textElement.onkeydown = (e) => {
+  console.log(e);
+};
